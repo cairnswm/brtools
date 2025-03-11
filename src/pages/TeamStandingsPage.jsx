cd6ed4eccb5f93b98c9775f6beff2c2f57f01463
William Cairns <cairnswm@gmail.com>
2025-03-11 16:38:19 +0200

    Change File name

--- src/pages/TeamStandingsPage.jsx
+++ src/pages/TeamStandingsPage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import TeamStandings from "./teamStandings"; 
+import TeamStandings from "./TeamStandingsTable"; 
 
 const TeamStandingsPage = () => {
   return (
